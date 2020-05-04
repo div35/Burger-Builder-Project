8bae7fe605430ac62008e3ca5e97f2bcef346fe5
Divyaansh <divyaansh@Divyaanshs-MacBook-Pro.local>
2020-05-04 21:50:31 +0530

    removed aux

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Aux from "./../../hoc/Aux";
 import classes from "./Layout.module.css";
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from "./../Navigation/SideDrawer/SideDrawer";
@@ -18,13 +17,13 @@ class Layout extends Component {
 
     render() {
         return (
-            <Aux>
+            <div>
                 <Toolbar showSideBar={this.showSideBarHandler}/>
                 <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
                 <main className={classes.Content}>
                     {this.props.children}
                 </main>
-            </Aux>
+            </div>
         );
     }
 };
